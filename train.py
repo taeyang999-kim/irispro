622e9ae4bb5d8d2bfd8a42746a9f6ffb7e0a1c00
taeyang999-kim <sunsun9579@naver.com>
2026-06-24 09:21:35 +0900

    123

--- train.py
+++ train.py
@@ -2,19 +2,8 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-# 데이터 로드
 iris = load_iris()
-
-X = iris.data
-y = iris.target
-
-# 모델 생성
 model = RandomForestClassifier()
+model.fit(iris.data, iris.target)
 
-# 학습
-model.fit(X, y)
-
-# 저장
 joblib.dump(model, "iris_model.pkl")
-
-print("모델 저장 완료")
